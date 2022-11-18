8f963b001a687f7e939d9721ce46844c3b531913
Carlos Alberto <95.carlos.velazquez@gmail.com>
2022-11-18 22:40:56 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon", "memes", "coronet"];
+  var extension = ["com", "net", "es"];
+
+  let domains = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < noun.length; y++) {
+        for (let z = 0; z < extension.length; z++) {
+          let aux = `${pronoun[i]}${adj[x]}${noun[y]}`;
+          if (aux.endsWith(extension[z])) {
+            domains.push(
+              `${aux.slice(0, -extension[z].length)}.${extension[z]}`
+            );
+          } else {
+            domains.push(`${aux}.${extension[z]}`);
+          }
+        }
+      }
+    }
+  }
+
+  console.log(`Options generated: ${domains.length}`);
+  domains.forEach(e => {
+    console.log(e);
+  });
 };
